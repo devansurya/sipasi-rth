4aeffefc4672bee92befa4576c6facd83821702d
devansurya <devannsuryaa26@gmail.com>
2023-11-15 17:59:02 +0700

    setup controller pengaduan

---
+++ application/controllers/Pengaduan.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Pengaduan extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('M_Dokumentasi');
+		$this->load->model('M_Guru');
+	}
+
+	public function index(){
+		$data['content'] = $this->load->view('admin/pengaduan', false, true);
+		$this->load->view('layouts-admin/index', $data);
+	}
+
+}
--- application/views/layouts-admin/sidebar.php
+++ application/views/layouts-admin/sidebar.php
@@ -36,7 +36,7 @@
                 <h6 class="lan-8">Applications</h6>
             </div>
             </li>
-            <li class="sidebar-list"><i class="fa fa-thumb-tack"></i><a class="sidebar-link sidebar-title link-nav" href="<?= base_url('Administrator/pengaduan')?>">
+            <li class="sidebar-list"><i class="fa fa-thumb-tack"></i><a class="sidebar-link sidebar-title link-nav" href="<?= base_url('Pengaduan')?>">
                 <svg class="stroke-icon">
                 <use href="<?= base_url(); ?>/assets-admin/svg/icon-sprite.svg#stroke-table"></use>
                 </svg>
