d1f38f712264d905dfda93110a217bbfa843199c
devansurya <devannsuryaa26@gmail.com>
2023-12-11 14:29:33 +0700

    breadcrumb detail pengaduan

--- application/views/publik/detail_pengaduan.php
+++ application/views/publik/detail_pengaduan.php
@@ -1,4 +1,16 @@
-
+<section class="wrapper bg-gray">
+  <div class="container py-3 py-md-5">
+    <nav class="d-inline-block" aria-label="breadcrumb">
+      <ol class="breadcrumb mb-0">
+        <li class="breadcrumb-item"><a href="<?= base_url('Home'); ?>">Home</a></li>
+        <li class="breadcrumb-item"><a href="<?= base_url('Home/pengaduan'); ?>">Pengaduan</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Detail</li>
+      </ol>
+    </nav>
+    <!-- /nav -->
+  </div>
+  <!-- /.container -->
+</section>
     <!-- /section -->
     <section class="wrapper bg-light">
       <div class="container py-14 py-md-16">
