f85d00cdfabc1f7861684f17397e36fead15447e
unknown <rifqyzanuar5@gmail.com>
2023-12-12 14:35:43 +0700

    Perbaikan registrasi dan kondisi status login

--- application/views/layouts-admin/header.php
+++ application/views/layouts-admin/header.php
@@ -29,31 +29,31 @@
                 </svg>
             </div>
             </li>
-            <li class="onhover-dropdown">
-            <div class="notification-box">
-                <svg>
-                <use href="<?= base_url(); ?>/assets-admin/svg/icon-sprite.svg#notification"></use>
-                </svg><span class="badge rounded-pill badge-secondary">4 </span>
-            </div>
-            <div class="onhover-show-div notification-dropdown">
-                <h6 class="f-18 mb-0 dropdown-title">Notitications                               </h6>
-                <ul>
-                <li class="b-l-primary border-4">
-                    <p>Delivery processing <span class="font-danger">10 min.</span></p>
-                </li>
-                <li class="b-l-success border-4">
-                    <p>Order Complete<span class="font-success">1 hr</span></p>
-                </li>
-                <li class="b-l-secondary border-4">
-                    <p>Tickets Generated<span class="font-secondary">3 hr</span></p>
-                </li>
-                <li class="b-l-warning border-4">
-                    <p>Delivery Complete<span class="font-warning">6 hr</span></p>
-                </li>
-                <li><a class="f-w-700" href="#">Check all</a></li>
-                </ul>
-            </div>
-            </li>
+            <!-- <li class="onhover-dropdown">
+                <div class="notification-box">
+                    <svg>
+                    <use href="<?= base_url(); ?>/assets-admin/svg/icon-sprite.svg#notification"></use>
+                    </svg><span class="badge rounded-pill badge-secondary">4 </span>
+                </div>
+                <div class="onhover-show-div notification-dropdown">
+                    <h6 class="f-18 mb-0 dropdown-title">Notitications                               </h6>
+                    <ul>
+                    <li class="b-l-primary border-4">
+                        <p>Delivery processing <span class="font-danger">10 min.</span></p>
+                    </li>
+                    <li class="b-l-success border-4">
+                        <p>Order Complete<span class="font-success">1 hr</span></p>
+                    </li>
+                    <li class="b-l-secondary border-4">
+                        <p>Tickets Generated<span class="font-secondary">3 hr</span></p>
+                    </li>
+                    <li class="b-l-warning border-4">
+                        <p>Delivery Complete<span class="font-warning">6 hr</span></p>
+                    </li>
+                    <li><a class="f-w-700" href="#">Check all</a></li>
+                    </ul>
+                </div>
+            </li> -->
             <li class="profile-nav onhover-dropdown pe-0 py-0">
             <div class="media profile-media"><img class="img-40 rounded-circle" src="<?= base_url('upload-profile/'. $profile['image'])?>" alt="">
                 <div class="media-body"><span><?= $profile['nama'] ? $profile['nama'] : '-'; ?></span>
