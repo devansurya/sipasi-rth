5451b1f4d14f3aad7bc8649405f2b0c0423891b3
rezziarfianc <rezziarfianc@gmail.com>
2024-05-10 00:43:39 +0700

    init api

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['api/v1/(.+)'] = '/api/v1/$1';
+$route['api/(.+)'] = '/api/v1/$1';
 $route['default_controller'] = 'Home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
