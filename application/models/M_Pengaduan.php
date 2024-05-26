2ef2dc41a8e1e07c95bc0a1f8e78d781fdc394b8
unknown <rifqyzanuar5@gmail.com>
2024-05-26 18:52:41 +0700

    detail pengaduan admin

--- application/models/M_Pengaduan.php
+++ application/models/M_Pengaduan.php
@@ -70,15 +70,13 @@ class M_Pengaduan extends CI_Model {
 
 	public function get_one($id= ''){
 
-		$this->db->select("p.*,sp.id_status, ct.nim,ct.email, kp.kategori, u.username, sp.status,ct.nama, ct.nama username,p.foto,v.visibilitas as privasi_status");
+		$this->db->select('p.*, jp.jenis_pengaduan, rth.nama_rth, v.visibilitas as privasi_status');
 		$this->db->from('pengaduan p');
-		$this->db->join('kategori_pengaduan kp', 'p.id_kategori = kp.id_kategori', 'left');
-		$this->db->join('status_pengaduan sp', 'sp.id_status = p.status', 'left');
+		$this->db->join('jenis_pengaduan jp', 'p.id_jenispengaduan = jp.id_jenispengaduan', 'left');
+		$this->db->join('rth', 'rth.id_rth = p.id_rth', 'left');
 		$this->db->join('visibilitas v', 'v.id_visibilitas = p.visibilitas', 'left');
-		$this->db->join('user u', 'u.id_user = p.id_user', 'left');
-		$this->db->join('contact ct', 'u.id_contact = ct.id_contact', 'left');
 		$this->db->where("p.id_pengaduan={$id}");
-		$this->db->order_by('p.created_at', 'desc');
+		$this->db->order_by('p.create_date', 'desc');
 
 		$data = $this->db->get()->row();
 
